c3809496128fadf5182b6aeaf8f82e40700530af
Dirk Theisen <dirk.theisen@glasfaserplus.de>
2026-01-14 22:21:50 +0100

    Add ESP-IDF v5.3.4 submodule and ESP32 size tooling
    Pin ESP-IDF as a git submodule (external/esp-idf) and add repo-local setup/env scripts so contributors can install and activate the toolchain reproducibly. Update the ESP32 CMake toolchain to prefer Xtensa tools from the ESP-IDF environment, and add a flash-tree size report script plus split archives (flashdb-core/flash-tree-core) for object-code accounting.

--- external/flash-tree/src/ft_bsd_btree.h
+++ external/flash-tree/src/ft_bsd_btree.h
@@ -37,6 +37,7 @@
  */
 
 #include "ft_bsd_mpool.h"
+#include <stdio.h>
 
 #define	DEFMINKEYPAGE	(2)		/* Minimum keys per page */
 #define	MINCACHE	(2)		/* Minimum cached pages (reduced for embedded) */
