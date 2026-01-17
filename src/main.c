b319c05e688c66a0fdb04fbffc35a1f3db6a1e14
Dirk Theisen <d.theisen@objectpark.org>
2026-01-17 02:17:11 +0100

    Rename autorelease pool cleanup API to drain
    Rename autorelease pool functions from cleanup_* to drain_* for clearer semantics and update call sites.

--- src/main.c
+++ src/main.c
@@ -47,7 +47,7 @@ int main() {
     
     // Cleanup
     meta_registry_cleanup(); // Cleanup meta functionality
-    autorelease_pool_cleanup_all(); // Cleanup alle Autorelease-Pools
+    autorelease_pool_drain_all(); // Drain alle Autorelease-Pools
     
     return 0;
 }
